ea6527ec0c4a68f41969ac14d93b39e49284e745
lohit_gb <lohitbhandari442@gmail.com>
2025-07-11 18:25:31 +0530

    updated

--- lib/health_sync_plugin.dart
+++ lib/health_sync_plugin.dart
@@ -7,3 +7,4 @@ export 'src/services/health_average_service.dart';
 export 'src/platform/health_connect_channel.dart';
 export 'src/services/foreground_task_service.dart';
 export 'src/background/health_sync_task_handler.dart';
+export 'src/services/health_history_service.dart';
