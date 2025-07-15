c08f5bd55448375d7a6a935caccb3e68ecdb099f
Lohitgb <lohitbhandarii442@gmail.com>
2025-07-15 17:15:23 +0530

    encryption added

--- lib/src/services/health_average_service.dart
+++ lib/src/services/health_average_service.dart
@@ -1,4 +1,5 @@
 import 'package:health/health.dart';
+import 'package:health_sync_plugin/src/helper/encrypt_decrypt_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -60,18 +61,32 @@ class HealthAverageService {
     // Calculate averages
     final Map<String, dynamic> avgMap = {};
 
-    void addAverage(String label, List<double> values) {
-      if (values.isNotEmpty) {
-        avgMap[label] = values.reduce((a, b) => a + b) / values.length;
-      }
-    }
+    // void addAverage(String label, List<double> values) {
+    //   if (values.isNotEmpty) {
+    //     avgMap[label] = values.reduce((a, b) => a + b) / values.length;
+    //   }
+    // }
 
     if (selectedHealthTypes.contains('HEART_RATE')) {
       final hr = filteredData
           .where((d) => d.type == HealthDataType.HEART_RATE)
           .map((d) => (d.value as NumericHealthValue).numericValue.toDouble())
           .toList();
-      addAverage('heart_rate_avg', hr);
+      if (hr.isNotEmpty) {
+        final heartRateAvg = hr.reduce((a, b) => a + b) / hr.length;
+
+        // Encrypt the average value as string
+        final encrypted =
+            await EncryptService.encryptText(heartRateAvg.toString());
+
+        // Store encrypted data and IV separately in Firestore-compatible format
+        avgMap['heart_rate_avg'] = {
+          'data': encrypted['data'],
+          'iv': encrypted['iv'],
+        };
+      }
+
+      // addAverage('heart_rate_avg', hr);
     }
 
     if (selectedHealthTypes.contains('STEPS')) {
@@ -79,7 +94,17 @@ class HealthAverageService {
           .where((d) => d.type == HealthDataType.STEPS)
           .map((d) => (d.value as NumericHealthValue).numericValue.toDouble())
           .toList();
-      addAverage('steps_avg', steps);
+      if (steps.isNotEmpty) {
+        final stepsAvg = steps.reduce((a, b) => a + b) / steps.length;
+        final encrypted = await EncryptService.encryptText(stepsAvg.toString());
+
+        avgMap['steps_avg'] = {
+          'data': encrypted['data'],
+          'iv': encrypted['iv'],
+        };
+      }
+
+      // addAverage('steps_avg', steps);
     }
 
     if (selectedHealthTypes.contains('BLOOD_PRESSURE')) {
@@ -92,8 +117,31 @@ class HealthAverageService {
           .map((d) => (d.value as NumericHealthValue).numericValue.toDouble())
           .toList();
 
-      addAverage('bp_systolic_avg', systolic);
-      addAverage('bp_diastolic_avg', diastolic);
+      if (systolic.isNotEmpty) {
+        final systolicAvg = systolic.reduce((a, b) => a + b) / systolic.length;
+        final encrypted =
+            await EncryptService.encryptText(systolicAvg.toString());
+
+        avgMap['bp_systolic_avg'] = {
+          'data': encrypted['data'],
+          'iv': encrypted['iv'],
+        };
+      }
+
+      if (diastolic.isNotEmpty) {
+        final diastolicAvg =
+            diastolic.reduce((a, b) => a + b) / diastolic.length;
+        final encrypted =
+            await EncryptService.encryptText(diastolicAvg.toString());
+
+        avgMap['bp_diastolic_avg'] = {
+          'data': encrypted['data'],
+          'iv': encrypted['iv'],
+        };
+      }
+
+      // addAverage('bp_systolic_avg', systolic);
+      // addAverage('bp_diastolic_avg', diastolic);
     }
 
     if (selectedHealthTypes.contains('BLOOD_GLUCOSE')) {
@@ -101,7 +149,18 @@ class HealthAverageService {
           .where((d) => d.type == HealthDataType.BLOOD_GLUCOSE)
           .map((d) => (d.value as NumericHealthValue).numericValue.toDouble())
           .toList();
-      addAverage('glucose_avg', glucose);
+      if (glucose.isNotEmpty) {
+        final glucoseAvg = glucose.reduce((a, b) => a + b) / glucose.length;
+        final encrypted =
+            await EncryptService.encryptText(glucoseAvg.toString());
+
+        avgMap['glucose_avg'] = {
+          'data': encrypted['data'],
+          'iv': encrypted['iv'],
+        };
+      }
+
+      // addAverage('glucose_avg', glucose);
     }
 
     // Store result in Firestore
